587599d2520af37cd96205bfb2bd83466c47135f
Rabibah Munir <135952028+Rabibah96@users.noreply.github.com>
2024-10-15 20:50:53 +0500

    Update and rename 14 Business Problems.sql to 15 Business Problems.sql

---
+++ 15 Business Problems.sql
@@ -0,0 +1,20 @@
+-- 14 Business Problems & Solutions
+
+1. Count and calculate the percentage of movies versus TV shows.
+2. Find the most common rating for movies and TV shows
+3. List all TV Shows released in a specific year(e.g., 2018)
+4. Find the top 5 countries with the most content on Disney
+5. Identify the longest movie on Disney
+6. Find Content Added in the Last 5 Years
+7. Find All Movies/TV Shows starred by 'Robert Downey Jr.'!
+8. List All TV Shows with More Than 5 Seasons
+9. Count the number of content items in each genre
+10.Find each year and the average numbers of content release in United States on Disney 
+return top 5 year with highest avg content release!
+11. List All Movies that are Documentaries
+12. Find All Content Without a Director
+13. Find How Many Movies director 'Anthony Russo' directed in the Last 10 Years
+14. Find the Top 10 Actors Who Have Appeared in the Highest Number of Movies Produced in United States
+15. Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+the description field. Label content containing these keywords as 'Bad' and all other 
+content as 'Good'. Count how many items fall into each category.
